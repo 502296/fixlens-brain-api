d5aee0f25482f39645d9c4bb8cb875979437647d
502296 <ali.kathem.edu@gmail.com>
2026-04-08 09:39:59 -0400

    Update search.js

--- search.js
+++ search.js
@@ -101,7 +101,7 @@ function normalizeText(value = "") {
   return String(value || "")
     .toLowerCase()
     .replace(/[\u2010-\u2015]/g, "-")
-    ..replace(/[^a-zA-Z0-9\s\-.,،]/g, " ")
+    .replace(/[^a-zA-Z0-9\s\-\.\,]/g, " ")
     .replace(/\s+/g, " ")
     .trim();
 }
